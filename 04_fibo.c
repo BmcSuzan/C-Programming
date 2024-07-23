461ad5dd6a8df57b5959bc3b14b6c157e49f2118
Sujan Tamang <sujanbcalab81@gmail.com>
2024-07-23 13:51:56 +0545

    fibo

--- 04_fibo.c
+++ 04_fibo.c
@@ -1,7 +1,18 @@
-#include<stdio.h>
-int main() {
- int i,a=1,b=0,c=0;
- printf("Enter the nth term");
- 
-return 0;
+#include <stdio.h>
+int main()
+{
+    int i, a = 1, b = 0, c = 0;
+    int num;
+    printf("Enter the nth term : ");
+    scanf("%d", &num);
+
+    for (i = 0; i < num; i++)
+    {
+        printf("%d\t", c);
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return 0;
 }
